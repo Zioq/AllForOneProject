be42e13464b4a34678ffe5cd487d8cbc4bfa358f
Zioq <jr.roberthan@gmail.com>
2020-10-28 11:19:17 -0700

    Finish Navigation with Grid design

---
+++ app/src/main/java/com/example/AllForOne/FindShelter.java
@@ -0,0 +1,14 @@
+package com.example.AllForOne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindShelter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_shelter);
+    }
+}
