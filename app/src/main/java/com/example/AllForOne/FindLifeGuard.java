84ddb80857045127be161d48b7b9cc0694273d16
Joan <joenahn934@gmail.com>
2020-10-29 18:56:08 -0700

    added files for signIn,logIn & lifeGuard list Not done code for detail functions

--- app/src/main/java/com/example/AllForOne/FindLifeGuard.java
+++ app/src/main/java/com/example/AllForOne/FindLifeGuard.java
@@ -2,13 +2,55 @@ package com.example.AllForOne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class FindLifeGuard extends AppCompatActivity {
+public class FindLifeGuard extends ListActivity {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        switch (position){
+            case 0:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.citycentreupcc.ca")));
+                break;
+            case 1:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vch.ca/Locations-Services/result?res_id=597")));
+                break;
+            case 2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vch.ca/Locations-Services/result?res_id=1438")));
+                break;
+            case 3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vch.ca/StaticDirectoryPages/573.aspx")));
+                break;
+            case 4:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https:///www.providencehealthcare.org/hospitals-residences/mount-saint-joseph-hospital")));
+                break;
+            case 5:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://http://www.vch.ca/Locations-Services/result?res_id=991")));
+                break;
+            case 6:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.providencehealthcare.org/hospitals-residences/st-paul%27s-hospital")));
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_life_guard);
+        //setContentView(R.layout.activity_find_life_guard);
+
+        String[] emergencies = {"City Center Urgent Primary care center", "Gordon & Leslie Diamond Helth Care Center",
+                "North Vancouver Urgent In Primary Care Center","Vancouver General Hospital Emergency Dept",
+                "Mount Saint Joseph Emergency Dept","Urgent Care Center-UBC","Saint Paul's Hospital Emergency"};
+
+        setListAdapter(new ArrayAdapter<String>(this,R.layout.activity_find_life_guard,
+                            R.id.lifeGuard, emergencies));
+
     }
 }
--- app/src/main/java/com/example/AllForOne/Login.java
+++ app/src/main/java/com/example/AllForOne/Login.java
@@ -2,7 +2,10 @@ package com.example.AllForOne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button logIn = (Button)findViewById(R.id.btnLog_in);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Navigation.class));
+            }
+        });
+
+        Button register = (Button)findViewById(R.id.btnRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, SignIn.class));
+            }
+        });
+
+        Button btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, FindLifeGuard.class));
+            }
+        });
     }
 }
