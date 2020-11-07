f963de65d4bfe3035750a37c4f00f9265002222c
Zioq <jr.roberthan@gmail.com>
2020-11-07 03:21:42 -0800

    Add database connection and add user register

---
+++ app/src/main/java/com/example/AllForOne/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.AllForOne;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "register.db";
+    public static final String TABLE_NAME = "register";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "UserName";
+    public static final String COL_3 = "UserId";
+    public static final String COL_4 = "UserPassword";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " +TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, UserId TEXT, UserPassword TEXT)" );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME );
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/AllForOne/Login.java
+++ app/src/main/java/com/example/AllForOne/Login.java
@@ -6,19 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText userNameInput;
+    EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        userNameInput = (EditText) findViewById(R.id.txt_id);
+        passwordInput = (EditText) findViewById(R.id.txt_password);
         Button logIn = (Button)findViewById(R.id.btnLog_in);
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this, Navigation.class));
+                String userName = userNameInput.getText().toString().trim();
+                String passsword = passwordInput.getText().toString().trim();
+
+
+                    startActivity(new Intent(Login.this, Navigation.class));
+
+                    //Toast.makeText(getApplicationContext(),"Check your Id and Password",Toast.LENGTH_LONG).show();
+
+
+
+
             }
         });
 
